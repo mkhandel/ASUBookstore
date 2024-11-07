9712cf20d7851b73c90d00d1557bb5c6c29c69e6
AEPullinger <89876443+AEPullinger@users.noreply.github.com>
2024-11-06 18:00:46 -0600

    Update User.java
    Added all getters and setters

--- User.java
+++ User.java
@@ -16,4 +16,46 @@ public class User {
 }
 	
 	public User() {}
+	
+	
+	public String getAccountType() {
+		return this.accountType;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	protected String getPassword() {
+		return this.password;
+	}
+	
+	public String getJoinDate() {
+		return this.joinDate;
+	}
+	
+	public void setAccountType(String type) {
+		this.accountType = type;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setJoinDate(String date) {
+		this.joinDate = date;
+	}
 }
+
