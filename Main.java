bfe8062b505e97cac9880569dc93c0845db1b0aa
alecmarkmorrisss <ammorr28@asu.edu>
2024-11-12 19:14:47 -0700

    Adding SellerController to the main

--- Main.java
+++ Main.java
@@ -31,6 +31,8 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} else if (thisUser.accountType.toLowerCase().contains("seller")) {
+			SellerController sellerController = new SellerController();
+			loader.setController(sellerController);
 			sceneController.activate("seller");
 			
 			primaryStage.setScene(scene);
