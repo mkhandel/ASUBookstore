71f2dda42cb775629d6572db59ad18459d8208e0
AEPullinger <89876443+AEPullinger@users.noreply.github.com>
2024-11-13 14:42:32 -0600

    Update SceneController.java
    Fixed spelling error

--- SceneController.java
+++ SceneController.java
@@ -1,29 +1,29 @@
-//Class for handing all of the programs screen changes
-//Call this to for User Interface Changes
 package application;
+
 
 import java.util.HashMap;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
-public class SceneController {
-    private HashMap<String, Pane> screenMap = new HashMap<>();
+public class SceneController extends AnchorPane{
+    private HashMap<String, Pane> sceneMap = new HashMap<>();
     private Scene main;
 
     public SceneController(Scene main) {
         this.main = main;
     }
 
-    protected void addScreen(String name, Pane pane){
-         screenMap.put(name, pane);
+    protected void addScene(String name, Pane pane){
+         sceneMap.put(name, pane);
     }
 
-    protected void removeScreen(String name){
-        screenMap.remove(name);
+    protected void removeScene(String name){
+        sceneMap.remove(name);
     }
 
     protected void activate(String name){
-        main.setRoot( screenMap.get(name) );
+        main.setRoot(sceneMap.get(name));
     }
 }
